f8abda11bacce92a47b7a038a5150a6fc5e73ee0
chiichen <chiichen@qq.com>
2023-11-08 00:03:38 +0800

    增加了对运算符的支持，但是存在栈递归错误的问题

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -1,13 +1,18 @@
+use std::ops::Neg;
+
 use super::string_parser::StringParser;
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::{self, complete::one_of},
-    combinator::{map, map_parser, map_res, recognize},
-    error::VerboseError,
+    character::{
+        self,
+        complete::{char, multispace0, one_of},
+    },
+    combinator::{cut, map, map_parser, map_res, recognize},
+    error::{context, VerboseError},
     multi::{many0, many1},
     number::complete::double,
-    sequence::{preceded, separated_pair, terminated},
+    sequence::{delimited, preceded, separated_pair, terminated},
     IResult, Parser,
 };
 //未附加属性的表达式
@@ -52,19 +57,80 @@ pub enum BuiltInOperator {
     /// `/`
     Div(Box<Expr>, Box<Expr>),
     /// `%`
-    Module(Box<Expr>),
+    Module(Box<Expr>, Box<Expr>),
     /// `&`
     BitAnd(Box<Expr>, Box<Expr>),
     /// `|`
     BitOr(Box<Expr>, Box<Expr>),
     /// `==`
     Equal(Box<Expr>, Box<Expr>),
+    /// `!=`
+    NotEqual(Box<Expr>, Box<Expr>),
 }
 
-impl BuiltInOperator {
-    // pub fn parse_built_in_operator(str:&str)->IResult<&str,BuiltInOperator,VerboseError<&str>>{
+impl<'a> BuiltInOperator {
+    pub fn parse_built_in_operator(
+        str: &str,
+    ) -> IResult<&str, BuiltInOperator, VerboseError<&str>> {
+        alt((
+            Self::parse_add_ops,
+            Self::parse_sub_ops,
+            Self::parse_mul_ops,
+            Self::parse_div_ops,
+            Self::parse_mod_ops,
+            Self::parse_bitand_ops,
+            Self::parse_bitor_ops,
+            Self::parse_equal_ops,
+            Self::parse_notequal_ops,
+        ))
+        .parse(str)
+    }
 
-    // }
+    fn parse_add_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, char('+'), Expr::parse_expr)
+            .map(|res| BuiltInOperator::Add(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_sub_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, char('-'), Expr::parse_expr)
+            .map(|res| BuiltInOperator::Sub(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_mul_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, char('*'), Expr::parse_expr)
+            .map(|res| BuiltInOperator::Mul(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_div_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, char('/'), Expr::parse_expr)
+            .map(|res| BuiltInOperator::Div(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_mod_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, char('%'), Expr::parse_expr)
+            .map(|res| BuiltInOperator::Module(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_bitand_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, char('&'), Expr::parse_expr)
+            .map(|res| BuiltInOperator::BitAnd(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_bitor_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, char('|'), Expr::parse_expr)
+            .map(|res| BuiltInOperator::BitOr(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_equal_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, tag("=="), Expr::parse_expr)
+            .map(|res| BuiltInOperator::Equal(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
+    fn parse_notequal_ops(str: &'a str) -> IResult<&'a str, BuiltInOperator, VerboseError<&str>> {
+        separated_pair(Expr::parse_expr, tag("!="), Expr::parse_expr)
+            .map(|res| BuiltInOperator::NotEqual(Box::new(res.0), Box::new(res.1)))
+            .parse(str)
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -81,9 +147,41 @@ pub enum Number {
     Float(f64),
 }
 
+impl Neg for Number {
+    type Output = Number;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            Number::Integer(data) => Number::Integer(-data),
+            Number::Float(data) => Number::Float(-data),
+        }
+    }
+}
+
 impl From<Number> for BuiltInType {
     fn from(value: Number) -> Self {
         BuiltInType::Num(value)
+    }
+}
+
+impl<'a> Expr {
+    pub fn parse_expr(str: &'a str) -> IResult<&'a str, Expr, VerboseError<&str>> {
+        alt((
+            delimited(
+                char('('),
+                preceded(multispace0, Self::parse_inner_expr),
+                context("closing paren", cut(preceded(multispace0, char(')')))),
+            ),
+            preceded(multispace0, Self::parse_inner_expr),
+        ))
+        .parse(str)
+    }
+    fn parse_inner_expr(str: &'a str) -> IResult<&'a str, Expr, VerboseError<&str>> {
+        alt((
+            BuiltInOperator::parse_built_in_operator.map(|res| Expr::Operator(res)),
+            BuiltInType::parse_built_in_type.map(|res| Expr::Literal(res)),
+        ))
+        .parse(str)
     }
 }
 
@@ -145,8 +243,9 @@ impl<'a> Number {
         .map(|result| (result.0, Number::Integer(result.1)))
     }
 
-    /// Parse the number with leading 0x/0X
-    pub fn parse_integer(str: &'a str) -> IResult<&'a str, Number, VerboseError<&str>> {
+    /// Parse a positive integer, that is the number without a leading "-" character
+    /// 分析一个没有前导负号的正数
+    pub fn parse_pos_integer(str: &'a str) -> IResult<&'a str, Number, VerboseError<&str>> {
         alt((
             Self::parse_hex,
             Self::parse_oct,
@@ -156,7 +255,16 @@ impl<'a> Number {
         .parse(str)
     }
 
-    pub fn parse_double(str: &'a str) -> IResult<&'a str, Number, VerboseError<&str>> {
+    /// Parse the number with leading 0x/0X
+    pub fn parse_integer(str: &'a str) -> IResult<&'a str, Number, VerboseError<&str>> {
+        alt((
+            Self::parse_pos_integer,
+            preceded(tag("-"), Self::parse_pos_integer).map(|res| -res),
+        ))
+        .parse(str)
+    }
+
+    pub fn parse_pos_double(str: &'a str) -> IResult<&'a str, Number, VerboseError<&str>> {
         map_parser(
             recognize(alt((
                 separated_pair(Self::parse_dec, tag("."), Self::parse_dec),
@@ -165,6 +273,14 @@ impl<'a> Number {
             ))),
             |_| double(str).map(|result| (result.0, Number::Float(result.1))),
         )
+        .parse(str)
+    }
+
+    pub fn parse_double(str: &'a str) -> IResult<&'a str, Number, VerboseError<&str>> {
+        alt((
+            Self::parse_pos_double,
+            preceded(tag("-"), Self::parse_pos_double).map(|res| -res),
+        ))
         .parse(str)
     }
 }
@@ -191,10 +307,10 @@ impl<'a> BuiltInType {
     }
 
     pub fn parse_built_in_type(str: &'a str) -> IResult<&'a str, BuiltInType, VerboseError<&str>> {
-        alt((
-            map(tag("true"), |_| BuiltInType::Boolean(true)),
-            map(tag("false"), |_| BuiltInType::Boolean(false)),
-        ))
+        context(
+            "parsing built-in type",
+            alt((Self::parse_string, Self::parse_num, Self::parse_boolean)),
+        )
         .parse(str)
     }
 }
@@ -218,6 +334,10 @@ mod tests {
             BuiltInType::Num(Number::Integer(4660))
         );
         assert_eq!(
+            BuiltInType::parse_num("-0x1234").unwrap().1,
+            BuiltInType::Num(Number::Integer(-4660))
+        );
+        assert_eq!(
             BuiltInType::parse_num("0b1111").unwrap().1,
             BuiltInType::Num(Number::Integer(15))
         );
@@ -226,8 +346,47 @@ mod tests {
             BuiltInType::Num(Number::Float(1.2e-11))
         );
         assert_eq!(
+            BuiltInType::parse_num("-12E-12").unwrap().1,
+            BuiltInType::Num(Number::Float(-1.2e-11))
+        );
+        assert_eq!(
             BuiltInType::parse_num("12K-03").unwrap().1,
             BuiltInType::Num(Number::Float(12.0))
         );
+    }
+
+    #[test]
+    fn test_built_in_type() {
+        assert_eq!(
+            BuiltInType::parse_built_in_type("0x1234").unwrap().1,
+            BuiltInType::Num(Number::Integer(4660))
+        );
+        assert_eq!(
+            BuiltInType::parse_built_in_type("0b1111").unwrap().1,
+            BuiltInType::Num(Number::Integer(15))
+        );
+        assert_eq!(
+            BuiltInType::parse_built_in_type("12E-12").unwrap().1,
+            BuiltInType::Num(Number::Float(1.2e-11))
+        );
+        assert_eq!(
+            BuiltInType::parse_built_in_type("12K-03").unwrap().1,
+            BuiltInType::Num(Number::Float(12.0))
+        );
+        assert_eq!(
+            BuiltInType::parse_built_in_type(r##""asdfa\"sdffuck'a'true""##)
+                .unwrap()
+                .1,
+            BuiltInType::String(r##"asdfa"sdffuck'a'true"##.to_string())
+        );
+        assert_eq!(
+            BuiltInType::parse_built_in_type("true").unwrap().1,
+            BuiltInType::Boolean(true)
+        );
+    }
+
+    #[test]
+    fn test_operator() {
+        println!("{:?}", Expr::parse_expr("1+2+(3+4)"))
     }
 }
