52959eab5874abaa732e048adec356c131cba4cc
= <chiichen@qq.com>
2023-11-13 17:22:14 +0800

    更新了代码的层次结构

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,9 @@
+use chumsky::span::SimpleSpan;
+
 pub mod expr;
-pub mod rcc_parser;
 pub mod string_parser;
-pub mod token;
-pub mod utils;
+type Span = SimpleSpan<usize>;
+type Spanned<T> = (T, Span);
+
+// 通过引入 nom 或者类似的 parser 框架，实际上是把 lexer和syntax analysis的工作一起做了
+pub struct RccParser;
