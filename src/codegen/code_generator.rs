a5aad897205817d07c36d04cc3592833beb0020f
Chiichen <39649411+Chiichen@users.noreply.github.com>
2023-11-13 20:12:38 +0800

    Migrate to chumsky (#5)
    * 增加了对运算符的支持，但是存在栈递归错误的问题
    
    * 迁移了部分词法分析的代码到 Chumsky 库中
    
    * 暂时无法解决递归的问题
    
    * 用 chumsky 重写了词法分析器，实现了错误处理与恢复，但是仍有部分逻辑待修改
    
    * 修复了一些错误，部分 clippy 报错是糊上去的
    
    * 更新了代码的层次结构
    
    * 修改了 deny.toml
    
    * 添加了 Unicode-DFS-2016 License
    
    * 修改 Unlicensed 为 deny
    
    ---------
    
    Co-authored-by: chiichen <chiichen@qq.com>

--- src/codegen/code_generator.rs
+++ src/codegen/code_generator.rs
@@ -1,8 +1,6 @@
-use std::{collections::HashMap, ffi::CString, ptr};
+use std::collections::HashMap;
 
-use llvm::prelude::{LLVMBuilderRef, LLVMContextRef, LLVMValueRef};
-
-use crate::parser::expr::Expr;
+use crate::parser::expr::Func;
 
 pub struct CodeGenerator;
 /*
@@ -13,107 +11,106 @@ impl CodeGenerator {
     /// # Safety
     ///
     /// This function should not be called before the horsemen are ready.
-    unsafe fn codegen_expr(
-        _context: LLVMContextRef,
-        _builder: LLVMBuilderRef,
-        _names: &mut HashMap<String, LLVMValueRef>,
-        expr: Expr,
-    ) -> LLVMValueRef {
-        match expr {
-            Expr::Literal(literal) => match literal {
-                crate::parser::expr::BuiltInType::Num(_) => todo!(),
-                crate::parser::expr::BuiltInType::String(_) => todo!(),
-                crate::parser::expr::BuiltInType::Boolean(_) => todo!(),
-            },
-            Expr::Ref(_) => todo!(),
-            Expr::Assign(_, _) => todo!(),
-            Expr::VarDeclaration(_, _) => todo!(),
-            Expr::Function(_, _) => todo!(),
-            Expr::Block(_) => todo!(),
-            Expr::IfBlock(_, _) => todo!(),
-            Expr::IfelseBlock(_, _, _) => todo!(),
-            Expr::WhileBlock(_, _) => todo!(),
-            Expr::Operator(_) => todo!(),
-        }
-        // match expr {
-        //     Expr::Literal(int_literal) => {
-        //         let int_type = llvm::core::LLVMInt64TypeInContext(context);
-        //         llvm::core::LLVMConstInt(int_type, int_literal.parse().unwrap(), 0)
-        //     }
+    // unsafe fn codegen_expr(
+    //     _context: LLVMContextRef,
+    //     _builder: LLVMBuilderRef,
+    //     _names: &mut HashMap<String, LLVMValueRef>,
+    //     _expr: Expr,
+    // ) -> LLVMValueRef {
+    //     todo!()
+    //     // match expr {
+    //     //     Expr::Error => todo!(),
+    //     //     Expr::Literal(_) => todo!(),
+    //     //     Expr::List(_) => todo!(),
+    //     //     Expr::Local(_) => todo!(),
+    //     //     Expr::Let(_, _, _) => todo!(),
+    //     //     Expr::Then(_, _) => todo!(),
+    //     //     Expr::Call(_, _) => todo!(),
+    //     //     Expr::Block(_) => todo!(),
+    //     //     Expr::If(_, _, _) => todo!(),
+    //     //     Expr::WhileBlock(_, _) => todo!(),
+    //     //     Expr::Operator(_) => todo!(),
+    //     // }
+    //     // match expr {
+    //     //     Expr::Literal(int_literal) => {
+    //     //         let int_type = llvm::core::LLVMInt64TypeInContext(context);
+    //     //         llvm::core::LLVMConstInt(int_type, int_literal.parse().unwrap(), 0)
+    //     //     }
 
-        //     Expr::Add(lhs, rhs) => {
-        //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
-        //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
+    //     //     Expr::Add(lhs, rhs) => {
+    //     //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
+    //     //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
 
-        //         let name = CString::new("addtmp").unwrap();
-        //         llvm::core::LLVMBuildAdd(builder, lhs, rhs, name.as_ptr())
-        //     }
+    //     //         let name = CString::new("addtmp").unwrap();
+    //     //         llvm::core::LLVMBuildAdd(builder, lhs, rhs, name.as_ptr())
+    //     //     }
 
-        //     Expr::Sub(lhs, rhs) => {
-        //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
-        //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
+    //     //     Expr::Sub(lhs, rhs) => {
+    //     //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
+    //     //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
 
-        //         let name = CString::new("subtmp").unwrap();
-        //         llvm::core::LLVMBuildSub(builder, lhs, rhs, name.as_ptr())
-        //     }
+    //     //         let name = CString::new("subtmp").unwrap();
+    //     //         llvm::core::LLVMBuildSub(builder, lhs, rhs, name.as_ptr())
+    //     //     }
 
-        //     Expr::Mul(lhs, rhs) => {
-        //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
-        //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
+    //     //     Expr::Mul(lhs, rhs) => {
+    //     //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
+    //     //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
 
-        //         let name = CString::new("multmp").unwrap();
-        //         llvm::core::LLVMBuildMul(builder, lhs, rhs, name.as_ptr())
-        //     }
+    //     //         let name = CString::new("multmp").unwrap();
+    //     //         llvm::core::LLVMBuildMul(builder, lhs, rhs, name.as_ptr())
+    //     //     }
 
-        //     Expr::Div(lhs, rhs) => {
-        //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
-        //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
+    //     //     Expr::Div(lhs, rhs) => {
+    //     //         let lhs = Self::codegen_expr(context, builder, names, *lhs);
+    //     //         let rhs = Self::codegen_expr(context, builder, names, *rhs);
 
-        //         let name = CString::new("divtmp").unwrap();
-        //         llvm::core::LLVMBuildUDiv(builder, lhs, rhs, name.as_ptr())
-        //     }
-        //     Expr::Ref(name) => *names.get(&name).unwrap(),
+    //     //         let name = CString::new("divtmp").unwrap();
+    //     //         llvm::core::LLVMBuildUDiv(builder, lhs, rhs, name.as_ptr())
+    //     //     }
+    //     //     Expr::Ref(name) => *names.get(&name).unwrap(),
 
-        //     Expr::Assign(name, expr) => {
-        //         let new_value = Self::codegen_expr(context, builder, names, *expr);
-        //         names.insert(name, new_value);
-        //         new_value
-        //     }
-        // }
-    }
+    //     //     Expr::Assign(name, expr) => {
+    //     //         let new_value = Self::codegen_expr(context, builder, names, *expr);
+    //     //         names.insert(name, new_value);
+    //     //         new_value
+    //     //     }
+    //     // }
+    // }
     /// # Safety
     ///
     /// This function should not be called before the horsemen are ready.
-    pub unsafe fn codegen(input: Vec<Expr>) {
-        let context = llvm::core::LLVMContextCreate();
-        let module = llvm::core::LLVMModuleCreateWithName(b"example_module\0".as_ptr() as *const _);
-        let builder = llvm::core::LLVMCreateBuilderInContext(context);
+    pub unsafe fn codegen(_input: (HashMap<&str, Func<'_>>, chumsky::span::SimpleSpan)) {
+        todo!()
+        // let context = llvm::core::LLVMContextCreate();
+        // let module = llvm::core::LLVMModuleCreateWithName(b"example_module\0".as_ptr() as *const _);
+        // let builder = llvm::core::LLVMCreateBuilderInContext(context);
 
-        // In LLVM, you get your types from functions.
-        let int_type = llvm::core::LLVMInt64TypeInContext(context);
-        let function_type = llvm::core::LLVMFunctionType(int_type, ptr::null_mut(), 0, 0);
-        let function =
-            llvm::core::LLVMAddFunction(module, b"main\0".as_ptr() as *const _, function_type);
+        // // In LLVM, you get your types from functions.
+        // let int_type = llvm::core::LLVMInt64TypeInContext(context);
+        // let function_type = llvm::core::LLVMFunctionType(int_type, ptr::null_mut(), 0, 0);
+        // let function =
+        //     llvm::core::LLVMAddFunction(module, b"main\0".as_ptr() as *const _, function_type);
 
-        let entry_name = CString::new("entry").unwrap();
-        let bb = llvm::core::LLVMAppendBasicBlockInContext(context, function, entry_name.as_ptr());
-        llvm::core::LLVMPositionBuilderAtEnd(builder, bb);
+        // let entry_name = CString::new("entry").unwrap();
+        // let bb = llvm::core::LLVMAppendBasicBlockInContext(context, function, entry_name.as_ptr());
+        // llvm::core::LLVMPositionBuilderAtEnd(builder, bb);
 
-        // The juicy part: construct a `LLVMValue` from a Rust value:
-        let zero = llvm::core::LLVMConstInt(int_type, 0, 0);
+        // // The juicy part: construct a `LLVMValue` from a Rust value:
+        // let zero = llvm::core::LLVMConstInt(int_type, 0, 0);
 
-        let mut names = HashMap::new();
-        let mut return_value = zero; // return value on empty program
-        for expr in input {
-            return_value = Self::codegen_expr(context, builder, &mut names, expr);
-        }
-        llvm::core::LLVMBuildRet(builder, return_value);
-        // Instead of dumping to stdout, let's write out the IR to `out.ll`
-        let out_file = CString::new("out.ll").unwrap();
-        llvm::core::LLVMPrintModuleToFile(module, out_file.as_ptr(), ptr::null_mut());
+        // let mut names = HashMap::new();
+        // let mut return_value = zero; // return value on empty program
+        // for expr in input {
+        //     return_value = Self::codegen_expr(context, builder, &mut names, expr);
+        // }
+        // llvm::core::LLVMBuildRet(builder, return_value);
+        // // Instead of dumping to stdout, let's write out the IR to `out.ll`
+        // let out_file = CString::new("out.ll").unwrap();
+        // llvm::core::LLVMPrintModuleToFile(module, out_file.as_ptr(), ptr::null_mut());
 
-        llvm::core::LLVMDisposeBuilder(builder);
-        llvm::core::LLVMDisposeModule(module);
-        llvm::core::LLVMContextDispose(context);
+        // llvm::core::LLVMDisposeBuilder(builder);
+        // llvm::core::LLVMDisposeModule(module);
+        // llvm::core::LLVMContextDispose(context);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,83 @@
 pub mod codegen;
 pub mod grammer;
+pub mod lexer;
 pub mod parser;
-
+extern crate ariadne;
+extern crate chumsky;
 extern crate llvm_sys as llvm;
 extern crate nom;
 
-use std::fs::File;
-use std::io::Read;
+use std::{env, fs};
 
+use ariadne::{sources, Color, Label, Report, ReportKind};
+use chumsky::{
+    prelude::{Input, Rich},
+    Parser,
+};
 use codegen::code_generator::CodeGenerator;
-use parser::rcc_parser::RccParser;
+use lexer::RccLexer;
+use parser::RccParser;
 
 fn main() {
-    let mut input = String::new();
-    let mut f = File::open("in.rcc").unwrap();
-    f.read_to_string(&mut input).unwrap();
+    let filename = env::args().nth(1).expect("Expected file argument");
 
-    // let parsed_input = parser::program(&input).unwrap();
+    let src = fs::read_to_string(&filename).expect("Failed to read file");
 
-    let r = RccParser::parse(&input);
-    if r.is_err() {
-        panic!("failed to parse file");
-    }
-    let parsed_input = r.unwrap();
+    let (tokens, mut errs) = RccLexer::lexer().parse(src.as_str()).into_output_errors();
+
+    let (ast, parse_errs) = if let Some(tokens) = &tokens {
+        let (ast, parse_errs) = RccParser::funcs_parser()
+            .map_with(|ast, e| (ast, e.span()))
+            .parse(tokens.as_slice().spanned((src.len()..src.len()).into()))
+            .into_output_errors();
+        if let Some((funcs, file_span)) = &ast {
+            if let Some(main) = funcs.get("main") {
+                if !main.args().is_empty() {
+                    errs.push(Rich::custom(
+                        main.span(),
+                        "The main function cannot have arguments".to_string(),
+                    ))
+                }
+            } else {
+                errs.push(Rich::custom(
+                    *file_span,
+                    "Programs need a main function but none was found".to_string(),
+                ));
+            }
+        }
+
+        (ast, parse_errs)
+    } else {
+        (None, Vec::new())
+    };
+
+    errs.into_iter()
+        .map(|e| e.map_token(|c| c.to_string()))
+        .chain(
+            parse_errs
+                .into_iter()
+                .map(|e| e.map_token(|tok| tok.to_string())),
+        )
+        .for_each(|e| {
+            Report::build(ReportKind::Error, filename.clone(), e.span().start)
+                .with_message(e.to_string())
+                .with_label(
+                    Label::new((filename.clone(), e.span().into_range()))
+                        .with_message(e.reason().to_string())
+                        .with_color(Color::Red),
+                )
+                .with_labels(e.contexts().map(|(label, span)| {
+                    Label::new((filename.clone(), span.into_range()))
+                        .with_message(format!("while parsing this {}", label))
+                        .with_color(Color::Yellow)
+                }))
+                .finish()
+                .print(sources([(filename.clone(), src.clone())]))
+                .unwrap()
+        });
     unsafe {
-        CodeGenerator::codegen(parsed_input);
+        CodeGenerator::codegen(
+            ast.unwrap_or_else(|| panic!("Failed to generate an ast on file {:?}", filename)),
+        )
     }
 }
